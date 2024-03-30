c003bdbb1ce62e37a2ea0c8820c40e158bbafe94
PArewa <bishal>
2024-03-30 12:02:41 +0545

    Added url to the navbar items

--- app/components/horizontal_nav_bar.tsx
+++ app/components/horizontal_nav_bar.tsx
@@ -4,20 +4,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAirbnb } from '@fortawesome/free-brands-svg-icons'
 import Horizontal_Nav_Bar_Item from './horizontal_nav_bar_item'
 import { user_name } from '../utils/airBnbConstants'
+import { DASHBOARD_URL } from '../utils/url'
 
 const Horizontal_Nav_Bar = () => {
   return (
     <div className='bg-white h-20 w-full pt-2 pb-2 flex items-center justify-between pr-12 pl-12'>
       <FontAwesomeIcon size='2xl' className='w-10 h-10 ' icon={faAirbnb} style={{ color: "#000000", }} />
       <div className='flex justify-between items-center h-full'>
-        <Horizontal_Nav_Bar_Item icon={faBars} />
+        <Horizontal_Nav_Bar_Item icon={faBars} url={DASHBOARD_URL} />
         <Horizontal_Nav_Bar_Item icon={faBars} />
         <Horizontal_Nav_Bar_Item icon={faBars} />
         <Horizontal_Nav_Bar_Item icon={faBars} />
       </div>
       <div className='flex justify-between items-center'>
         <div className='w-44 text-right'>{user_name}</div>
-        <div className='bg-usa rounded-full w-14 h-14 bg-cover'></div>
+        <div className='bg-usa rounded-full w-14 h-14 bg-cover ring-2 ring-black ml-2'></div>
 
       </div>
     </div>
--- app/components/horizontal_nav_bar_item.tsx
+++ app/components/horizontal_nav_bar_item.tsx
@@ -1,14 +1,18 @@
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
 import React from 'react'
 
-const Horizontal_Nav_Bar_Item = ({icon = faBars}) => {
+const Horizontal_Nav_Bar_Item = ({ icon = faBars, url = "" }) => {
   return (
-    <div className='group flex flex-col justify-between items-center h-full mr-24'>
-      <div></div>
-      <FontAwesomeIcon icon={icon} className= "" style={{ color: "#000000", width: "20px", height: "20px", }} />
-      <div className='group-hover:bg-black h-1 w-8 rounded-xl'></div>
-    </div>
+    <Link href={url}>
+      <div className='group flex flex-col justify-between items-center h-full mr-24 hover:cursor-pointer'>
+        <div></div>
+        <FontAwesomeIcon icon={icon} className="" style={{ color: "#000000", width: "20px", height: "20px", }} />
+        <div className='group-hover:bg-black h-1 w-8 rounded-xl'></div>
+      </div>
+    </Link>
+
   )
 }
 
